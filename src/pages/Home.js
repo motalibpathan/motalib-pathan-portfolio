a32b270f0f577e20f1eafe3fc837454ca9398c82
motalibpathan11 <73628059+motalibpathan11@users.noreply.github.com>
2022-06-06 11:57:15 +0600

    changes home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TypeAnimation from "react-type-animation";
 import "./Home.css";
 
 const Home = () => {
@@ -11,12 +12,38 @@ const Home = () => {
         <div className="md:ml-10 ml-5">
           <p className="text-gray-400">WELCOME TO MY WORLD</p>
           <h1 className="text-3xl md:text-7xl font-bold mt-3">
-            Hi, I'm John Doe
+            Hi, I'm Motalib Pathan
           </h1>
           <h2 className="text-3xl md:text-6xl font-bold md:my-4 my-0 text-rose-600">
-            JS Developer
+            <TypeAnimation
+              cursor={true}
+              sequence={[
+                " JS Developer",
+                2000,
+                " Front End Developer",
+                2000,
+                " React Developer",
+                2000,
+                " MERN Stack Developer",
+                2000,
+                " Graphics Designer",
+                2000,
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              className="after:text-white after:content-['|']"
+            />
           </h2>
-          <h4 className="text-xl md:text-5xl font-bold">based in USA.</h4>
+          {/* <h4 className="text-xl md:text-5xl font-bold">based in USA.</h4> */}
+          <div className="mt-10">
+            <a
+              href="#contact"
+              className="hover:text-white border-2 border-rose-600 bg-rose-600 py-3 px-9 cursor-pointer rounded-md mt-10
+          bg-transparent text-rose-600 duration-500"
+            >
+              Hire Me
+            </a>
+          </div>
         </div>
       </div>
     </header>
