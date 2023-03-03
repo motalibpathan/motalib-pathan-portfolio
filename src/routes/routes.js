b01cafafe72880c9a6fb9630069c1d1b5a8f950b
2019000010081 <motalibpathan11@gmail.com>
2023-03-03 16:17:09 +0600

    ui version 2 updated v2

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
+import ProjectDetails from "../pages/ProjectDetails";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/project/:id",
+    element: <ProjectDetails />,
   },
 ]);
 
